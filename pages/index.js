176581b0035c8a6b906369f1d22c975654ab5095
Cian Dolphin-Murray <dolphinmurraycian@gmail.com>
2021-03-09 21:50:27 +0000

    some meta tags

--- pages/index.js
+++ pages/index.js
@@ -23,6 +23,8 @@ export default function Home({ projects }) {
     <>
       <Head>
         <title>Home | Cian Dolphin-Murray</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta charSet="utf-8" />
       </Head>
       <HomeHero />
       <Skills />
