0783d379eaaecbbbc004894a9786f26ddd837ecc
Cian Dolphin-Murray <dolphinmurraycian@gmail.com>
2021-03-10 07:51:41 +0000

    Removing slick dependencies

--- src/components/Carousel/Container/index.js
+++ src/components/Carousel/Container/index.js
@@ -1,6 +1,4 @@
 import React from 'react';
-import 'slick-carousel/slick/slick.css';
-import 'slick-carousel/slick/slick-theme.css';
 import * as S from './styled';
 
 const Container = ({ children }) => {
