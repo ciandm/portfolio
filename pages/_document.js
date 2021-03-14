d6018d06971f2c3013cb607f646914d83220e0d4
Cian Dolphin-Murray <dolphinmurraycian@gmail.com>
2021-03-14 08:58:56 +0000

    Added GTM

--- pages/_document.js
+++ pages/_document.js
@@ -30,8 +30,30 @@ class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
-        <Head />
+        <Head>
+          {/* Google Tag Manager */}
+          <script
+            // eslint-disable-next-line
+            dangerouslySetInnerHTML={{
+              __html: `(function(w,d,s,l,i){w[l] = w[l] || []{'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-5QLQDNS');`,
+            }}
+          />
+          {/* End of Google Tag Manager */}
+        </Head>
         <body>
+          {/* Google Tag Manager noscript */}
+          <noscript
+            // eslint-disable-next-line
+            dangerouslySetInnerHTML={{
+              __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5QLQDNS"
+              height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
+            }}
+          />
+          {/* End Google Tag Manager noscript */}
           <Main />
           <NextScript />
         </body>
